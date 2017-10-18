413ddd70c27626079bd42a52fb0f7b3060391b68
Sérgio Matheus <matheus_imagina@hotmail.com>
2017-10-18 17:29:53 -0300

    Commit inicial de Questionarios

--- app/src/main/java/com/example/pichau/appmetadados/MainActivity.java
+++ app/src/main/java/com/example/pichau/appmetadados/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pichau.appmetadados;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button botao = (Button)findViewById(R.id.button2);
+        Button botao2=(Button)findViewById(R.id.button3);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CriaQuestionarios.class);
+                startActivity(i);
+            }
+        });
+        botao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, visualizar_activity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
